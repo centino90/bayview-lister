be13beef02a8bb5e83b050a4e9b99961f62961ba
centino90 <anthonyjayansit2326282@gmail.com>
2021-01-17 22:09:34 +0800

    qwe

--- laravel_lister.sql
+++ laravel_lister.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2021 at 05:29 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Jan 17, 2021 at 02:56 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -25,12 +25,48 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `searchRecords` (IN `fln` VARCHAR(70), IN `lim` INT(11))  SELECT full_name(fname, mname, lname) AS FullName, created_at FROM residents WHERE CONCAT(fname, ' ' ,mname, ' ', lname) LIKE fln LIMIT lim$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getDailyCertificationReport` (IN `class` VARCHAR(50))  BEGIN
+
+    if (class = 'countAllByRow') THEN
+        select count(id) AS count FROM residents WHERE date(created_at) = CURDATE();
+    ELSEIF (class = 'countAllByIssue') THEN
+       select issue, count(issue) AS count FROM residents WHERE date(created_at) = CURDATE() GROUP BY issue;
+	ELSEIF (class = 'countAllByName') THEN
+		select full_name(fname, mname, lname) as FullName, count(id) as count from residents WHERE date(created_at) = CURDATE() GROUP BY full_name(fname, mname, lname);    
+    END IF;
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getMonthlyCertificationReport` (IN `class` VARCHAR(50))  BEGIN
+
+    if (class = 'countAllByRow') THEN
+        select count(id) AS count FROM residents where month(created_at) = month(curdate());
+    ELSEIF (class = 'countAllByIssue') THEN
+       select issue, count(issue) AS count FROM residents where month(created_at) = month(curdate()) GROUP BY issue;
+	ELSEIF (class = 'countAllByName') THEN
+		select full_name(fname, mname, lname) as FullName, count(id) as count from residents where month(created_at) = month(curdate()) GROUP BY full_name(fname, mname, lname);    
+    END IF;
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getWeeklyCertificationReport` (IN `class` VARCHAR(50))  BEGIN
+
+    if (class = 'countAllByRow') THEN
+        select count(id) AS count FROM residents where week(created_at) = week(curdate());
+    ELSEIF (class = 'countAllByIssue') THEN
+       select issue, count(issue) AS count FROM residents where week(created_at) = week(curdate()) GROUP BY issue;
+	ELSEIF (class = 'countAllByName') THEN
+		select full_name(fname, mname, lname) as FullName, count(id) as count from residents where week(created_at) = week(curdate()) GROUP BY full_name(fname, mname, lname);    
+    END IF;
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `searchRecords` (IN `fln` VARCHAR(70), IN `lim` INT(11))  SELECT full_name(fname, mname, lname) AS FullName, issue, purpose, created_at FROM residents WHERE CONCAT(fname, ' ' ,mname, ' ', lname) LIKE fln LIMIT lim$$
 
 --
 -- Functions
 --
-CREATE DEFINER=`root`@`localhost` FUNCTION `full_name` (`fname` VARCHAR(50), `mname` CHAR(1), `lname` VARCHAR(50)) RETURNS VARCHAR(100) CHARSET utf8mb4 RETURN CONCAT(fname, ' ' , mname, ' ', lname)$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `full_name` (`fname` VARCHAR(50), `mname` CHAR(1), `lname` VARCHAR(50)) RETURNS VARCHAR(100) CHARSET utf8mb4 RETURN CONCAT(fname, " " , mname, " ", lname)$$
 
 DELIMITER ;
 
@@ -67,10 +103,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(8, '2014_10_12_000000_create_users_table', 1),
-(9, '2014_10_12_100000_create_password_resets_table', 1),
-(10, '2019_08_19_000000_create_failed_jobs_table', 1),
-(11, '2021_01_14_122112_add_updatedat_to_residents_table', 1);
+(47, '2014_10_12_000000_create_users_table', 1),
+(48, '2014_10_12_100000_create_password_resets_table', 1),
+(49, '2019_08_19_000000_create_failed_jobs_table', 1),
+(50, '2021_01_14_122112_add_updatedat_to_residents_table', 1);
 
 -- --------------------------------------------------------
 
@@ -87,37 +123,31 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `qweqwe`
---
-
-CREATE TABLE `qweqwe` (
-  `id` int(11) NOT NULL,
-  `qweqwe` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `residents`
 --
 
 CREATE TABLE `residents` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `fname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `mname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `lname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `fname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `mname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `lname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `issue` varchar(255) COLLATE utf8_bin NOT NULL,
+  `purpose` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `residents`
 --
 
-INSERT INTO `residents` (`id`, `fname`, `mname`, `lname`, `created_at`, `updated_at`) VALUES
-(1, 'qwe', 'w', 'qwe', '2021-01-16 03:44:04', NULL),
-(2, 'anthon', 'p', 'ansit', '2021-01-16 04:24:55', NULL),
-(3, 'anthony', 'p', 'ansi', '2021-01-16 04:25:18', NULL);
+INSERT INTO `residents` (`id`, `fname`, `mname`, `lname`, `issue`, `purpose`, `created_at`, `updated_at`) VALUES
+(5, 'qwe', 'q', 'qwe', 'qweqw', 'eqwe', '2021-01-17 06:50:20', NULL),
+(6, 'Anthony', 'p', 'Ansit', 'foodpacks', NULL, '2021-01-17 07:00:25', NULL),
+(7, 'jerry', 's', 'ansit', 'travel pass', NULL, '2021-01-17 09:24:08', NULL),
+(8, 'bryan', 'p', 'Ansit', 'foodpacks', NULL, '2021-01-17 09:25:33', NULL),
+(9, 'angelita', 'p', 'ansit', 'foodpacks', NULL, '2021-01-17 09:26:07', NULL),
+(10, 'Anthony', 'p', 'Ansit', 'foodpacks', NULL, '2021-02-11 11:36:22', NULL);
 
 -- --------------------------------------------------------
 
@@ -158,12 +188,6 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
--- Indexes for table `qweqwe`
---
-ALTER TABLE `qweqwe`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `residents`
 --
 ALTER TABLE `residents`
@@ -189,19 +213,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `qweqwe`
---
-ALTER TABLE `qweqwe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `residents`
 --
 ALTER TABLE `residents`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
